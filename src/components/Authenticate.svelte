9f9db8eb5d357ddd748321ace3c0f3c6ee5fec07
Luis Ruiz <ruiztechservices@gmail.com>
2023-09-24 08:43:32 -0400

    designed form and dashboard

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,174 @@
+<script>
+    let email;
+    let password;
+    let confirmPass;
+    let error = false;
+    let register = false;
+
+    function handleAuthenticate() {
+        if (!email || !password || (register && !confirmPass)) {
+            error=true;
+            return
+        }
+    }
+
+    function handleRegister() {
+        register = !register;
+    }
+</script>
+
+<div class="authContainer">
+    <form action="">
+        <h1>
+            {register ? "Register" : "Login"}
+        </h1>
+        {#if error}
+        <p class="error">The information you entered is not correct.</p>
+        {/if}
+        <label><p class="{email ? ' above': ' center'}">Email</p><input bind:value={email} type="email" placeholder="email"></label>
+        <label><p class="{password ? ' above': ' center'}">Password</p><input bind:value={password} type="password" placeholder="password"></label>
+        {#if register}
+        <label><p class="{confirmPass ? ' above': ' center'}">Confirm Password</p><input bind:value={confirmPass} type="password" placeholder="confirm password"></label>
+        {/if}
+        <button type="button">Submit</button>
+    </form>
+    <div class="options">
+        <p>Or</p>
+        {#if register}
+        <div>
+            <p>Already have an account?</p>
+            <p on:click={handleRegister} on:keydown={()=>{}}>Login</p>
+        </div>
+        {:else}
+        <div>
+            <p>Don't have an account?</p>
+            <p on:click={handleRegister} on:keydown={()=>{}}>Register</p>
+        </div>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .authContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex:1;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 14px;        
+    }
+    form, .options{
+        width: 400px;
+        max-width: 100%;
+        margin: 0 auto;
+    }
+    form input{
+        width: 100%;
+    }
+    h1{
+        text-align: center;
+        font-size: 3rem;
+    }
+    form label{
+        position: relative;
+        border: 1px solid navy;
+        border-radius: 5px;
+    }
+    form input{
+        border: none;
+        background: transparent;
+        color: white;
+        padding: 14px;
+        border-radius: 5px;
+    }
+    form input:focus{
+        border: none;
+        outline: none;
+        border-radius: 5px;
+    }
+    form label:focus-within{
+        border-color: blue;
+    }
+    form button{
+        background: navy;
+        color: white;
+        border: none;
+        padding: 10px 0;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 1rem;
+    }
+    form button:hover{
+        background: blue;
+    }
+    .above, .center{
+        position: absolute;
+        transform: translateY(-50%);
+        pointer-events: none;
+        color: white;
+        border-radius: 4px;
+        padding: 0 6px;
+        font-size: 0.8rem;
+    }
+    .above{
+        top: 0;
+        left: 24px;
+        background: navy;
+        border: 1px solid blue;
+        font-size: 0.7rem;
+    }
+    .center{
+        top: 50%;
+        left: 6px;
+        border: 1px solid transparent;
+        opacity: 0 auto;
+    }
+    .error{
+        color: coral;
+        font-size: 0.9rem;
+    }
+    .options{
+        padding: 14px 0;
+        overflow: hidden;
+        font-size: 0.9rem;
+        display: flex;
+        flex-direction: column;
+        gap:4px;
+    }
+    .options>p{
+        position: relative;
+        text-align: center;
+        width: fit-content;
+        margin: 0 auto;
+        padding: 0 8px;
+    }
+    .options>::after, .options>::before{
+        position: absolute;
+        content: '';
+        top: 50%;
+        transform: translateY(-50%);
+        width: 100vw;
+        height: 1.5px;
+        background: white;
+    }
+    .options>::after{
+        right: 100%;
+    }
+    .options>::before{
+        left: 100%;
+    }
+    .options div{
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        justify-content: center;
+    }
+    .options div p:last-of-type{
+        color: cyan;
+        cursor: pointer;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="mainContainer">
+    <slot/>    
+</div>
+<style>
+    .mainContainer{
+        min-height: 100vh;
+        background: linear-gradient(to right, #020627, #000046);
+        color: white;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
